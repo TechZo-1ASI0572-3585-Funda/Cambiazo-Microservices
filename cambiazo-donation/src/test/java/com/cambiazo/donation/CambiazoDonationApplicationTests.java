55eaf3f26d0e474aaba05b50a9aa7609fc53ed4e
Joseph Huamani | Nerk <josephhuamanilol@gmail.com>
2025-06-22 20:43:31 -0500

    feat: added donation context

---
+++ cambiazo-donation/src/test/java/com/cambiazo/donation/CambiazoDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambiazo.donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambiazoDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
